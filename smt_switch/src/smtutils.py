a469139d90fe8e7f264dbf70cec8772b844d0bc5
makaimann <makaim@stanford.edu>
2017-05-04 15:37:22 -0700

    Using getattr

--- smt_switch/src/smtutils.py
+++ smt_switch/src/smtutils.py
@@ -1,4 +1,3 @@
-from . import terms
 from . import sorts
 
 
@@ -7,7 +6,6 @@ def sorts_list(consts):
        Returns a list of sorts based on the input consts.
        If sort for a particular const is unknown, replaces element with None.
     '''
-    return list(map(lambda arg:
-                    arg.sort if issubclass(arg.__class__, terms.TermBase)
-                    else sorts.py2sort[type(arg)]() if type(arg) in sorts.py2sort
-                    else None, consts))
+    return list(map(lambda arg: getattr(arg, 'sort',
+                    sorts.py2sort[type(arg)]() if type(arg) in sorts.py2sort
+                    else None), consts))
